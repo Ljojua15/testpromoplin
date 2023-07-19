e3c1fe0f759626dd08f6cbea1d794489f00be399
Ljojua15 <97795530+Ljojua15@users.noreply.github.com>
2023-07-19 17:28:52 +0400

    sadfasdf

---
+++ script.js
@@ -0,0 +1,20 @@
+// const nika = {
+//   brand: `subaru`,
+//   model: `forester`,
+//   year: `2020`,
+//   color: `white`,
+//   madeIn: `usa`,
+//   location: `rustavi`,
+// };
+
+// for (const property in nika) {
+//   console.log(`პარამეტრენი - ${property} შემადგენოლობა ${nika[property]}`);
+// }
+
+const car = [`lasha`, `lasha1`, `lasha2`, `lasha3`];
+
+// for (i = 1; i < car.length; i++) {
+//   console.log(car[i]);
+// }
+
+let i = 0;
