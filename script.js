9ecaf53cb33331f132f637db8be83df147eeabf0
Ljojua15 <97795530+Ljojua15@users.noreply.github.com>
2023-07-24 15:16:37 +0400

    asdasdfasd

--- script.js
+++ script.js
@@ -17,9 +17,34 @@ const car = [`lasha`, `lasha1`, `lasha2`, `lasha3`];
 //   console.log(car[i]);
 // }
 
-let i = 0;
+// let i = 0;
 
-while (i < car.length) {
-  console.log(car[i]);
-  i++;
-}
+// while (i < car.length) {
+//   console.log(car[i]);
+//   i++;
+// }
+
+// for (let cars of car) {
+//   console.log(cars);
+// }
+
+// for (let nnn of car) {
+//   if (nnn == `lasha2`) continue ;
+//   console.log(nnn);
+// }
+
+// car.forEach((name, index) => {
+//   console.log(`his name is ${name}, index is ${index}`);
+// });
+
+car.forEach((cars) => console.log(`his name is ${car}`));
+
+const user = {
+  saxeli: `lasha`,
+  gvari: `jojua`,
+  sruliSaxeli() {
+    return `${this.saxeli} ${this.gvari}`;
+  },
+};
+
+console.log(user.sruliSaxeli());
